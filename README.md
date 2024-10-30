5fef8619b08db895988f2e8f2355d7437ec6d47b
ajmal31 <ajmalmuhammed846@gmail.com>
2024-10-30 16:30:58 +0530

    Refactor : README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# For Running project on your system
+ - git clone https://github.com/ajmal31/DocuVille_Document_Extractor_Server.git
+ - npm install 
+ - npm run dev -> http://localhost:5000/
--- src/app.js
+++ src/app.js
@@ -10,9 +10,10 @@ app.use(helmet())
 
 app.use(morgan("dev"))
 
-app.use(cors({origin:"http://localhost:5173"}))
+app.use(cors({origin:["http://localhost:5173","http://localhost:3000"]}))
 
 app.use("/api/v1",routes(express))
+
 app.listen(PORT,()=>{
     console.warn(`server Listening on port ${PORT}`)
 })
